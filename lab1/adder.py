5cd0746246bdccd24a20d54eb614e117d19cc839
Saikaeo <saikaeo.c@kkumail.com>
2023-11-17 17:24:02 +0700

    Update adder.py

--- lab1/adder.py
+++ lab1/adder.py
@@ -1,18 +1,13 @@
 # adder.py
-
+# Get the first decimal number from the user
 number1 = float(input("Enter a number: "))
-
-
+# Get the second decimal number from the user
 number2 = float(input("Enter a number: "))
-
-
+# Add the two numbers
 result = number1 + number2
-
+# Display the result
 print(f"{number1} + {number2} = {result:.2f}")
 
 
 
-# Get the first decimal number from the user
-# Get the second decimal number from the user
-# Add the two numbers
-# Display the result
+
