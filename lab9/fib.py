a8c026b8d3e329360d558b51001eae9df806e43d
SAIKAEO <102976104+SAIKAEO@users.noreply.github.com>
2024-02-25 20:15:25 +0700

    Add files via upload

---
+++ lab9/fib.py
@@ -0,0 +1,12 @@
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n-2) + fib(n-1)
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) > 1:
+        print(fib(int(sys.argv[1])))
